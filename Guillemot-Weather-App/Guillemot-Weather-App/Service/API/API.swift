9eab0733c7bc36a518657f64a4f8fdd48b70e71e
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-10-13 09:08:31 +0300

    tiny improvements

--- Guillemot-Weather-App/Guillemot-Weather-App/Service/API/API.swift
+++ Guillemot-Weather-App/Guillemot-Weather-App/Service/API/API.swift
@@ -9,4 +9,9 @@ enum API: String, CaseIterable {
     static var env = API.dev
 
     case dev = "https://api.weather.gov"
+    case image = "https://picsum.photos"
+
+    func imageURL(index: Int) -> String {
+        return "\(String(describing: API.image.rawValue))/\(index)"
+    }
 }
